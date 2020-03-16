ffe4b7ec0254d33ef3bc6f249ab6e2a4a67850e2
Irasema0915 <angelaruiz.0915@gmail.com>
2020-03-16 02:14:31 -0600

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms'
+import { RouterModule, Routes} from '@angular/router'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +10,20 @@ import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RegisterComponent } from './components/register/register.component';
+import { CancionesComponent } from './components/canciones/canciones.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AuthenticationService} from './authentication.service'
+import { AuthGuardService} from './auth-guard.service';
+import { from } from 'rxjs';
+
+const routes = Routes = [
+  {path: '', component: DashboardComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register ', component: RegisterComponent},
+  {path: 'profile ', component: ProfileComponent,
+      canActivate: [AuthGuardService]}
+]
+
 
 @NgModule({
   declarations: [
@@ -14,13 +31,18 @@ import { RegisterComponent } from './components/register/register.component';
     LoginComponent,
     DashboardComponent,
     NavbarComponent,
-    RegisterComponent
+    RegisterComponent,
+    CancionesComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
-  providers: [],
+  providers: [AuthGuardService, AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
