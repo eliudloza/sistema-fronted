c3fc5d5278dbdc6252c2979c48bd0b66ebf60190
eliud loza <eliudlozautt@hotmail.com>
2020-03-17 09:53:43 -0600

    Agregue google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
-import { RouterModule, Routes} from '@angular/router'
-
 import { AppRoutingModule } from './app-routing.module';
+import {SocialLoginModule, AuthServiceConfig} from 'angularx-social-login';
+import {GoogleLoginProvider} from 'angularx-social-login';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -16,6 +17,18 @@ import { ConocenosComponent } from './components/conocenos/conocenos.component';
 import {SpotifyService} from './services/spotify.service';
 import { from } from 'rxjs';
 
+const config = new AuthServiceConfig([
+  {
+    id: GoogleLoginProvider.PROVIDER_ID,
+    provider: new GoogleLoginProvider("75533910497-esahaqu5rvlsr9rg7s73tmkg9en0irpg.apps.googleusercontent.com")
+  }
+]);
+
+export function provideConfig() {
+  return config;
+}
+
+
 
 @NgModule({
   declarations: [
@@ -26,16 +39,20 @@ import { from } from 'rxjs';
     RegisterComponent,
     CancionesComponent,
     ConocenosComponent,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    SocialLoginModule
   ],
   providers: [
-    SpotifyService
+    SpotifyService,
+    {
+      provide: AuthServiceConfig,
+      useFactory: provideConfig
+    }
   ],
   bootstrap: [AppComponent]
 })
